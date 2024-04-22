ae393a1c084e6924999cfbda8373c6f64ba22f72
MESUT KAYA <MESUT.KAYA@lcwaikiki.com>
2024-04-22 19:13:10 +0300

    Add project files.

---
+++ CalculateNote/Ogrenci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateNote
+{
+    public class Ogrenci
+    {
+        private string ad;
+        private int numara;
+        private double vizeNotu;
+        private double finalNotu;
+
+        // Constructor
+        public Ogrenci(string ad, int numara, double vizeNotu, double finalNotu)
+        {
+            this.ad = ad;
+            this.numara = numara;
+            this.vizeNotu = vizeNotu;
+            this.finalNotu = finalNotu;
+        }
+
+        // Not hesaplama methodu
+        private double NotHesapla()
+        {
+            return (vizeNotu * 0.4) + (finalNotu * 0.6);
+        }
+
+        // Öğrenci bilgilerini yazdıran method
+        public void OgrenciYazdir()
+        {
+            double ortalama = NotHesapla();
+            Console.WriteLine($"Ogrenci Bilgileri: Adı: {ad}, Numarası: {numara}, Not Ortalaması: {ortalama}");
+        }
+    }
+}
---
+++ CalculateNote/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CalculateNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "student.txt";
+            string[] satirlar = null;
+            List<Ogrenci> ogrenciler = new List<Ogrenci>();
+            try
+            {
+                 satirlar = File.ReadAllLines(path);
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Dosya Okunamadı:");
+                Console.WriteLine(e.Message); 
+            }
+        
+                foreach (string satir in satirlar)
+            {
+                string[] bilgiler = satir.Split(' ');
+                string ad = bilgiler[0];
+                int numara = int.Parse(bilgiler[1]);
+                double vizeNotu = double.Parse(bilgiler[2]);
+                double finalNotu = double.Parse(bilgiler[3]);
+                Ogrenci ogrenci = new Ogrenci(ad, numara, vizeNotu, finalNotu);
+                ogrenciler.Add(ogrenci);
+            }
+
+            // Öğrenci bilgilerini yazdırma
+            foreach (Ogrenci ogrenci in ogrenciler)
+            {
+                ogrenci.OgrenciYazdir();
+            }
+        }
+    }
+}
